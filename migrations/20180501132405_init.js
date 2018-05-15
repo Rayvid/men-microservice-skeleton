b98e2b0240160fbf4f5137c8f582412cb2ce0319
Rayvid <gin.kisonas@gmail.com>
2018-05-15 17:35:31 +0300

    file left uncommited

--- migrations/20180501132405_init.js
+++ migrations/20180501132405_init.js
@@ -5,7 +5,8 @@ exports.up = function (knex, Promise) {
         table.integer('status').comment('1 - pending, 2 - accepted, 3 - persisted to blockchain, 4 - rejected ... add more on will').notNullable().defaultTo(1);
         table.string('payload', 65535).comment('any JSON attached to transaction').nullable();
         table.string('audit_trail', 65535).comment('JSON array, of events and their results [{"type":"event type-name","payload":{...}},...]').nullable();
-        table.timestamps();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at');
     });
 };
 
