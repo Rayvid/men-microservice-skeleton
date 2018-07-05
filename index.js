863a5ce76cbdda4a58067b826eefdd1f5ff755fe
Rayvid <gin.kisonas@gmail.com>
2018-07-05 20:47:41 +0300

    remove env config from index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const dotenv = require('dotenv');
-const fs = require('fs');
-const path = require('path');
-
-const secretEnv = process.env.ENV_FILE || '/run/secrets/env';
-const secretPath = path.resolve(secretEnv);
-
-if (fs.existsSync(secretPath)) {
-  dotenv.config({ path: secretPath });
-} else if (process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: path.resolve('dev.env') });
-}
-
 require('./src');
