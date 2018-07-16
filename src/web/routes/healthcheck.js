e0a7f15e81c543ab82d6f7c60ba9c864f6d547fe
Rayvid <gin.kisonas@gmail.com>
2018-07-16 11:01:30 +0300

    healtcheck.js - Mongo connecticity check template added

--- src/web/routes/healthcheck.js
+++ src/web/routes/healthcheck.js
@@ -1,3 +1,5 @@
 module.exports = async (req, res) => {
+  // Uncomment next line if you need Mongo connectivity check
+  // res.locals.getModels();
   res.status(200).json({ status: 'healthy' });
 };
