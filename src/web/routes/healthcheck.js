c43a8d7a34cb8a331f4f7561e01692a06d532ec8
Rayvid <gin.kisonas@gmail.com>
2018-07-16 12:34:03 +0300

    healthcheck: update due to async dbmanager

--- src/web/routes/healthcheck.js
+++ src/web/routes/healthcheck.js
@@ -1,5 +1,15 @@
+// Uncomment if you need Mongo connectivity check
+// const log = require('../../util').logger;
+
 module.exports = async (req, res) => {
-  // Uncomment next line if you need Mongo connectivity check
-  // res.locals.getModels();
+  // Uncomment if you need Mongo connectivity check
+  // try {
+  //   await res.locals.getModels();
+  // } catch (err) {
+  //   log.error('Service unhealthy!', err);
+  //   res.status(500).json({ status: 'unhealthy' });
+  //   return;
+  // }
+
   res.status(200).json({ status: 'healthy' });
 };
