14ac20582e3b321ac071ccd575a23eb35667fc2a
Rayvid <gin.kisonas@gmail.com>
2018-07-24 16:36:11 +0300

    sentry: remove env var validation

--- config/index.js
+++ config/index.js
@@ -3,12 +3,11 @@ const joi = require('joi');
 const server = require('./schema/server');
 const elastic = require('./schema/elastic');
 const logger = require('./schema/logger');
-const sentry = require('./schema/sentry');
 
 module.exports = {
   db: config.get('db'),
   server: joi.attempt(config.get('server'), server),
   elastic: joi.attempt(config.get('elastic'), elastic),
   logger: joi.attempt(config.get('logger'), logger),
-  sentry: joi.attempt(config.get('sentry'), sentry),
+  sentry: config.get('sentry'),
 };
