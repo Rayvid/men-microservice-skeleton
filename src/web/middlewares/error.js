804f26235d267a31509f62075ed5f4bbab059304
Rayvid <gin.kisonas@gmail.com>
2018-07-29 18:10:26 +0300

    error.js: statusCode and fields added

--- src/web/middlewares/error.js
+++ src/web/middlewares/error.js
@@ -4,12 +4,16 @@ const log = require('../../util').logger;
 module.exports = (err, req, res, next) => {
   /* eslint-enable no-unused-vars */
   log.error(
-    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
-    err.stack ? `\n${err.stack}` : '\n<No error stack available>',
+    `${err.status || err.statusCode || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+    [ // This is to log error in one batch (avoid making multiple log.error calls)
+      // (becomes important when sending to networked sinks)
+      (err.fields) ? `fields: ${JSON.stringify(err.fields)}` : undefined,
+      err.stack,
+    ].reduce((result, _) => result + ((_) ? `\n${_}` : '')),
   );
 
   const errorObj = { message: err.message };
-  res.status(err.status || 500);
+  res.status(err.status || err.statusCode || 500);
   if (err.stack && process.env.NODE_ENV !== 'production') {
     errorObj.stack = err.stack;
   }
