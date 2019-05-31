7894d70517a7f00772dcc393df401c9cabf9bb90
CodeLegend620 <50647994+CodeLegend620@users.noreply.github.com>
2019-05-31 11:24:23 +0800

    winston to pino

--- src/web/middlewares/index.js
+++ src/web/middlewares/index.js
@@ -1,5 +1,5 @@
 const { logger: log } = require('../../util');
-const expressWinston = require('express-winston');
+const expressPinoLogger = require('express-pino-logger');
 const modelInitializer = require('./modelInitializer');
 const bodyParser = require('body-parser');
 const errorHandler = require('./error.js');
@@ -7,7 +7,7 @@ const notFoundHandler = require('./404.js');
 
 module.exports = {
   beforeHandler: [
-    app => app.use(expressWinston.logger(log)),
+    app => app.use(expressPinoLogger({ logger: log })),
     app => app.use(bodyParser.json({ limit: '10mb' })),
     modelInitializer],
   notFound: notFoundHandler,
