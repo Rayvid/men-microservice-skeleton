9aa516cdb03ec9ef8d20923aec7cf91ed89832f2
CodeLegend620 <50647994+CodeLegend620@users.noreply.github.com>
2019-05-31 11:22:48 +0800

    winston to pino

--- src/util/logger/logger.js
+++ src/util/logger/logger.js
@@ -1,38 +1,16 @@
-const winston = require('winston');
+const pino = require('pino');
+const path = require('path');
+const config = require('../../../config');
 const defaultOptions = require('./options');
-const DailyRotateFile = require('winston-daily-rotate-file');
+
+const logDir = path.join(process.cwd(), config.logger.dir);
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const init = (options = defaultOptions) => {
-  const transports = [
-    new DailyRotateFile(options.file),
-    new winston.transports.Console(options.console),
-  ];
-  // seems deprecated forever
-  // if (options.elastic.clientOpts.host) {
-  // transports.push(new ElasticSearch(options.elastic));
-  // }
-  const logger = winston.createLogger({
-    transports,
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.timestamp(),
-      winston.format.printf(info =>
-        /* Logic is actually quite simple - if message is string try to check object itself
-          if it has exception fields. If it does not - just output message */
-        // eslint-disable-next-line no-nested-ternary
-        `${info.timestamp} ${info.level}: ${typeof info.message !== 'string'
-          ? info.message.inspect
-            ? info.message.inspect()
-            : JSON.stringify(info.message)
-          // eslint-disable-next-line no-nested-ternary
-          : info.stack || info.fields
-            ? info.inspect
-              ? info.inspect()
-              : JSON.stringify(info)
-            : info.message}`),
-    ),
-    exitOnError: false,
-  });
+
+  const logger = pino(options, pino.destination(path.join(logDir, "combined.log")));
 
   return logger;
 };
--- src/util/logger/options.js
+++ src/util/logger/options.js
@@ -1,34 +1,8 @@
-const path = require('path');
-const fs = require('fs');
-const config = require('../../../config');
-
-const logDir = path.join(process.cwd(), config.logger.dir);
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
 
 const options = {
-  file: {
-    level: config.logger.level.file,
-    filename: path.join(logDir, config.logger.fileName),
-    handleExceptions: true,
-    datePattern: 'YYYY-MM-DD',
-    maxSize: '5m',
-    maxFiles: '5d',
-    colorize: false,
-  },
-  elastic: {
-    level: config.logger.level.elastic,
-    messageType: '_doc',
-    clientOpts: { host: (config.elastic) ? config.elastic.host : undefined },
-    handleExceptions: true,
-    colorize: false,
-  },
-  console: {
-    level: config.logger.level.console,
-    handleExceptions: true,
-    colorize: true,
-  },
-};
+  name: 'men-microservice-skeleton',
+  level: 'debug',
+  prettyPrint: true
+}
 
 module.exports = options;
