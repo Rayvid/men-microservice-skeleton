2c976d34d2e05f9f394ecc0a6b2e63e62aafa261
Rayvid <gin.kisonas@gmail.com>
2019-10-18 06:38:51 +0300

    add option to not augment stack to the base Exception class

--- src/exceptions/Exception.js
+++ src/exceptions/Exception.js
@@ -12,9 +12,8 @@ class Exception extends Error {
       message: 'Exception occured',
       statusCode: 500,
       innerError: undefined,
-      fields: undefined,
-      // Read about fields purpose in REST communication there:
-      // https://lympodev.atlassian.net/wiki/spaces/LYMP/pages/193560577/REST+communication+standards
+      fields: undefined, // Server validation scenrios and similar, to show field specific issues
+      augmentStack: true // False will save resources if you use throw instead return (which isn't recommended)
     },
   ) {
     let constructorParameters = defaultParams;
@@ -23,36 +22,38 @@ class Exception extends Error {
     }
     super(constructorParameters.message || defaultParams.message);
 
-    // Capturing stack trace and excluding constructor call from it.
-    // This requires some explanation i believe:
-    // implementing exception bubbling trough async code is still
-    // painfull in Node. So we are using stacktrace concatenation to workaround that
-    //
-    // So error handling pattern becomes like:
-    // try {
-    //   await someAsyncCode...
-    // } catch (err) {
-    //   throw new Exception(err);
-    // }
-    //
-    // It works with promises too:
-    // new Promise((success, failure) => {
-    //   originalPromise.then(_ => success(_)).catch(err => failure(new Exception(err)));
-    // });
-    Error.captureStackTrace(this, this.constructor);
-    if (params && params.stack) {
-      this.stack += `\n${params.stack}`;
-      if (params.message) {
-        this.cause = params; // 4 Sentry to see it
-      }
-    } else if (constructorParameters.innerError && constructorParameters.innerError.stack) {
-      this.stack += `\n${constructorParameters.innerError.stack}`;
-      if (constructorParameters.innerError.message) {
-        this.cause = constructorParameters.innerError; // 4 Sentry to see it
+    if (augmentStack) {
+      // Capturing stack trace and excluding constructor call from it.
+      // This requires some explanation i believe:
+      // implementing exception bubbling trough async code is still
+      // painfull in Node. So we are using stacktrace concatenation to workaround that
+      //
+      // So error handling pattern becomes like:
+      // try {
+      //   await someAsyncCode...
+      // } catch (err) {
+      //   throw new Exception(err);
+      // }
+      //
+      // It works with promises too:
+      // new Promise((success, failure) => {
+      //   originalPromise.then(_ => success(_)).catch(err => failure(new Exception(err)));
+      // });
+      Error.captureStackTrace(this, this.constructor);
+      if (params && params.stack) {
+        this.stack += `\n${params.stack}`;
+        if (params.message) {
+          this.cause = params; // 4 Sentry to see it
+        }
+      } else if (constructorParameters.innerError && constructorParameters.innerError.stack) {
+        this.stack += `\n${constructorParameters.innerError.stack}`;
+        if (constructorParameters.innerError.message) {
+          this.cause = constructorParameters.innerError; // 4 Sentry to see it
+        }
       }
     }
 
-    // Saving namespace for exception type comparison (Comparing types itself is tricky in Node)
+    // Saving namespace for exception type comparison (comparing types itself is tricky in Node)
     this.name = `${pkgJson.name.toUpperCase()}.${this.constructor.name}`;
 
     // Most commonly it will be HTTP status,
