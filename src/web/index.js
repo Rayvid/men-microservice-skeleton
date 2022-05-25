8932bbd573d3043b19896846b122460650592fe5
Rayvid <gin.kisonas@gmail.com>
2022-05-25 09:09:51 +0300

    Discount api mock

--- src/web/index.js
+++ src/web/index.js
@@ -31,6 +31,8 @@ app.get('/health', routes.healthCheckRoutes.healthCheck);
 app.get('/health/sentry', routes.healthCheckRoutes.sentryPing);
 app.get('/version', routes.versionCheck);
 
+app.get('/getPriceAndWalletsByDiscount', routes.getPriceAndWalletsByDiscount);
+
 middlewares.afterHandler.forEach((_) => _(app));
 
 app.listen(config.server.listenOnPort, () => {
