9a0cbfcaf22291a832c514d6c8006e43ce494e69
Adomas Alimas <adomas204@gmail.com>
2022-05-31 21:20:59 +0300

    cleanup + comments

--- src/web/routes/transactionCheck.js
+++ src/web/routes/transactionCheck.js
@@ -1,5 +1,4 @@
 import * as exceptions from '../../exceptions/index.js';
-import { Connection, PublicKey } from '@solana/web3.js';
 
 export const getTransactions = async (req, res) => {
     const models = await res.locals.getModels();
@@ -29,62 +28,3 @@ export const getTransactions = async (req, res) => {
     }
 };
 
-
-async function getTransactionsOfUser(walletAddress, options, connection){
-    const USDCmint = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
-    const USDTmint = "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB";
-
-    try {
-        const publicKey = new PublicKey(walletAddress);
-        const transSignatures = await connection.getConfirmedSignaturesForAddress2(publicKey, options);
-        const transactions = [];
-
-        for (let i = 0; i < transSignatures.length; i++) {
-            const signature = transSignatures[i].signature;
-            const confirmedTransaction = await connection.getConfirmedTransaction(signature);
-
-            if(!confirmedTransaction) {
-                continue;
-            }
-
-            const { meta } = confirmedTransaction;
-
-            if (!meta){
-                continue;
-            }
-            
-            let solChange = 0;
-            let usdcChange = 0;
-            let usdtChange = 0;
-
-            if (meta.postTokenBalances.length === 0){
-                const oldBalance = meta.preBalances;
-                const newBalance = meta.postBalances;
-                solChange = oldBalance[0] - newBalance[0];
-            } else if (meta.preTokenBalances[0] != undefined) {
-                if(meta.preTokenBalances[0].mint === USDCmint) {
-                    usdcChange = meta.preTokenBalances[0].uiTokenAmount.uiAmount - meta.postTokenBalances[0].uiTokenAmount.uiAmount;
-                }
-                if(meta.preTokenBalances[0].mint === USDTmint) {
-                    usdcChange = meta.preTokenBalances[0].uiTokenAmount.uiAmount - meta.postTokenBalances[0].uiTokenAmount.uiAmount;
-                }
-            }
-
-            const transWithSignature = {
-                signature,
-                walletAddress,
-                authority: "",
-                blockTime: confirmedTransaction.blockTime,
-                solChange,
-                usdcChange,
-                usdtChange
-                };
-                transactions.push(transWithSignature);
-        }
-
-        return transactions;
-
-    } catch (err) {
-        throw (err);
-    }
-};
