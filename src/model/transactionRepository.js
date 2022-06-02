a0e9c61f293e555dfdd4006b60714c75d7d13a85
Adomas Alimas <adomas204@gmail.com>
2022-06-02 18:45:17 +0300

    lint fixes

--- src/model/transactionRepository.js
+++ src/model/transactionRepository.js
@@ -1,15 +1,15 @@
 import * as schema from './schema/index.js';
 
 export default (dbConnection) => ({
-    getTransaction: async (walletAddress, limitNum = 10, lean = true) => {
-        const result = schema.transaction
-            .connect(dbConnection).find({ walletAddress: walletAddress }).limit(limitNum);
+  getTransaction: async (walletAddress, limitNum = 10, lean = true) => {
+    const result = schema.transaction
+        .connect(dbConnection).find({walletAddress: walletAddress}).limit(limitNum);
 
-        return (lean) ? result.lean() : result;
-    },
+    return (lean) ? result.lean() : result;
+  },
 
-    createTransaction: async (transaction) => {
-        schema.transaction.connect(dbConnection)
+  createTransaction: async (transaction) => {
+    schema.transaction.connect(dbConnection)
         .create(transaction);
-    },
-})
+  },
+});
--- src/web/routes/transactionCheck.js
+++ src/web/routes/transactionCheck.js
@@ -1,23 +1,23 @@
 import * as exceptions from '../../exceptions/index.js';
 
 export const getTransactions = async (req, res) => {
-    const models = await res.locals.getModels();
-    let limit = 10
+  const models = await res.locals.getModels();
+  let limit = 10;
 
-    if(req.query.last) {
-        if (!isFinite(req.query.last)){
-            // TODO: does this need to throw an error, or just continue execution with
-            // the default limit = 10
-            throw new exceptions.Exception({message: "last query can only be numeric", innerError: null});
-        }
-        limit = parseInt(req.query.last);
+  if (req.query.last) {
+    if (!isFinite(req.query.last)) {
+      // TODO: does this need to throw an error, or just continue execution with
+      // the default limit = 10
+      throw new exceptions.Exception({message: 'last query can only be numeric', innerError: null});
     }
+    limit = parseInt(req.query.last);
+  }
 
-    try {
-        const transactions = await models.transaction.getTransaction(req.params.address, limit);
-        res.status(200).json(transactions)
-    } catch (err) {
-        throw new exceptions.Exception({message: "transaction query failed", innerError: err});
-    }
+  try {
+    const transactions = await models.transaction.getTransaction(req.params.address, limit);
+    res.status(200).json(transactions);
+  } catch (err) {
+    throw new exceptions.Exception({message: 'transaction query failed', innerError: err});
+  }
 };
 
