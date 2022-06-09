e9781d7faf96562ca8d9ab43f8ece7cfb88adbc3
Rayvid <gin.kisonas@gmail.com>
2022-06-09 12:27:14 +0300

    Tiny fixes of no real importance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import {logger as log} from './util/index.js';
+import {Exception} from './exceptions/index.js';
 
 process.on('uncaughtException', (err) => {
-  log.error(err);
+  log.error(new Exception({message: 'UNHANDLED-ERROR', innerError: err}));
   process.exit(1);
 });
 
 import './web/index.js';
-
-
